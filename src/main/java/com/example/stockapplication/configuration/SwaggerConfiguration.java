25d4f283452b6d1da74bd128c5a19ae7bb307b08
hungphunghuy <hungphung2000@gmail.com>
2022-11-07 20:43:48 +0700

    swagger-config

---
+++ src/main/java/com/example/stockapplication/configuration/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.stockapplication.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder().group("StockApp").pathsToMatch("/api/**").build();
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(new Info().title("NCC StockApp Api").contact(new Contact().name("NCC")).version("1.0.0"));
+    }
+}
